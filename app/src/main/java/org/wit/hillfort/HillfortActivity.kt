06f54a271bc70afcf6e4ad928d64040c655d2175
unknown <kacperwoloszyn4@gmail.com>
2018-10-12 11:01:57 +0100

    Updating build, colors, strings, xml file and app

--- app/src/main/java/org/wit/hillfort/HillfortActivity.kt
+++ app/src/main/java/org/wit/hillfort/HillfortActivity.kt
@@ -2,8 +2,9 @@ package org.wit.hillfort
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import org.jetbrains.anko.AnkoLogger
 
-class HillfortActivity : AppCompatActivity() {
+class HillfortActivity : AppCompatActivity(), AnkoLogger {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
