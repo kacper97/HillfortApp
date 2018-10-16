f72500c6c39259300e1a8f7337c83a1fa83fa7e9
unknown <kacperwoloszyn4@gmail.com>
2018-10-16 01:37:17 +0100

    adding cardview+recycler +updating mainapp and list activity

--- app/src/main/java/org/wit/hillfort/activities/HillfortListActivity.kt
+++ app/src/main/java/org/wit/hillfort/activities/HillfortListActivity.kt
@@ -3,7 +3,15 @@ package org.wit.hillfort.activities
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import org.wit.hillfort.R
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.activity_hillfort_list.*
+import kotlinx.android.synthetic.main.card_hillfort.view.*
 import org.wit.hillfort.main.MainApp
+import org.wit.hillfort.models.HillfortModel
 
 class HillfortListActivity : AppCompatActivity() {
 
@@ -13,5 +21,32 @@ class HillfortListActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_hillfort_list)
     app = application as MainApp
+
+    val layoutManager=LinearLayoutManager(this)
+    recyclerView.layoutManager=layoutManager
+    recyclerView.adapter=HillfortAdapter(app.hillforts)
   }
 }
+
+class HillfortAdapter constructor(private var hillforts: List<HillfortModel>) : RecyclerView.Adapter<HillfortAdapter.MainHolder>() {
+
+  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainHolder {
+    return MainHolder(LayoutInflater.from(parent?.context).inflate(R.layout.card_hillfort, parent, false))
+  }
+
+  override fun onBindViewHolder(holder: MainHolder, position: Int) {
+    val hillfort = hillforts[holder.adapterPosition]
+    holder.bind(hillfort)
+  }
+
+  override fun getItemCount(): Int = hillforts.size
+
+  class MainHolder constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    fun bind(hillfort: HillfortModel) {
+      itemView.hillfortTitle.text = hillfort.title
+      itemView.description.text = hillfort.description
+    }
+  }
+}
+
