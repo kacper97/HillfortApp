a02a7e77a5f1d7a61ff0e3690c5dc243165e612d
unknown <kacperwoloszyn4@gmail.com>
2018-10-16 01:12:14 +0100

    adding main package and updating methods

--- app/src/main/java/org/wit/hillfort/activities/HillfortActivity.kt
+++ app/src/main/java/org/wit/hillfort/activities/HillfortActivity.kt
@@ -7,24 +7,26 @@ import org.jetbrains.anko.info
 import kotlinx.android.synthetic.main.activity_hillfort.*
 import org.jetbrains.anko.toast
 import org.wit.hillfort.R
+import org.wit.hillfort.main.MainApp
 import org.wit.hillfort.models.HillfortModel
 
 class HillfortActivity : AppCompatActivity(), AnkoLogger {
 
   var hillfort = HillfortModel()
-  val hillforts = ArrayList<HillfortModel>()
+  lateinit var app : MainApp
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_hillfort)
+    app = application as MainApp
 
     btnAdd.setOnClickListener(){
       hillfort.title = hillfortTitle.text.toString()
       hillfort.description = description.text.toString()
       if (hillfort.title.isNotEmpty()) {
-        hillforts.add(hillfort.copy())
+        app.hillforts.add(hillfort.copy())
         info("add Button Pressed: $hillfortTitle")
-        hillforts.forEach{info("add Button Pressed: ${it.title}")}
+        app.hillforts.forEach{info("add Button Pressed: ${it.title}")}
       }
       else {
         toast ("Please Enter a title")
