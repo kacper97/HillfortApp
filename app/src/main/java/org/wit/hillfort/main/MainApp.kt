e0b2e30f5ec7b6d1b6d4cf3f86661151cf1e044a
unknown <kacperwoloszyn4@gmail.com>
2018-10-19 09:34:41 +0100

    Adding unstaged files, working from that step onwards

--- app/src/main/java/org/wit/hillfort/main/MainApp.kt
+++ app/src/main/java/org/wit/hillfort/main/MainApp.kt
@@ -13,7 +13,7 @@ class MainApp : Application(), AnkoLogger {
     super.onCreate()
     info("Hillfort started")
     //hillforts.add(HillfortModel("One", "About one..."))
-   // hillforts.add(HillfortModel("Two", "About two..."))
+    //hillforts.add(HillfortModel("Two", "About two..."))
     //hillforts.add(HillfortModel("Three", "About three..."))
   }
 }
