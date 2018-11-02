edca9825a99cc29b0787fa70765d06ef5eb08c48
unknown <kacperwoloszyn4@gmail.com>
2018-11-02 01:37:15 +0000

    Starting log in/ log out/ sign in / out feature

---
+++ app/src/main/java/org/wit/hillfort/activities/LogInActivity.kt
@@ -0,0 +1,28 @@
+package org.wit.hillfort.activities
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_hillfort.*
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
+import org.jetbrains.anko.toast
+import org.wit.hillfort.main.MainApp
+import org.wit.hillfort.models.UserModel
+import org.wit.hillfort.R
+
+class LogInActivity:  AppCompatActivity(), AnkoLogger {
+
+  var user = UserModel()
+  lateinit var app : MainApp
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_login)
+    app = application as MainApp
+
+    btnAdd.setOnClickListener() {
+        toast ("Please Enter an email")
+      }
+    }
+  }
+
