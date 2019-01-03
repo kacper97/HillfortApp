dbef93798c4ed48e82333f4622a0f0aaa2bf3aa6
unknown <kacperwoloszyn4@gmail.com>
2019-01-03 03:39:32 +0000

    Settings as in template

--- app/src/main/java/org/wit/hillfort/views/settings/SettingsPresenter.kt
+++ app/src/main/java/org/wit/hillfort/views/settings/SettingsPresenter.kt
@@ -1,2 +1,27 @@
 package org.wit.hillfort.views.settings
 
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import org.jetbrains.anko.toast
+import org.wit.hillfort.R
+import org.wit.hillfort.models.UserModel
+import org.wit.hillfort.views.BasePresenter
+import org.wit.hillfort.views.BaseView
+
+class SettingsPresenter(view: BaseView) : BasePresenter(view){
+
+  var auth: FirebaseAuth = FirebaseAuth.getInstance()
+
+  fun doSaveSettings(settings_email : String, settings_password : String){
+
+    if (settings_email.isEmpty() || settings_password.isEmpty()){
+      view?.toast(R.string.toast_changed_cant_be_blank)
+    } else {
+      view?.toast("Cant change these with firebase yet")
+    }
+
+    view?.setResult(AppCompatActivity.RESULT_OK)
+    view?.finish()
+  }
+
+}
--- app/src/main/java/org/wit/hillfort/views/settings/SettingsView.kt
+++ app/src/main/java/org/wit/hillfort/views/settings/SettingsView.kt
@@ -1,2 +1,48 @@
 package org.wit.hillfort.views.settings
 
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_setting.*
+import org.wit.hillfort.R
+import org.wit.hillfort.main.MainApp
+import org.wit.hillfort.views.BaseView
+
+
+class SettingsView : BaseView() {
+
+  lateinit var presenter: SettingsPresenter
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_setting)
+    setSupportActionBar(toolbarSettings)
+    supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+    presenter = initPresenter (SettingsPresenter(this)) as SettingsPresenter
+
+    var hillfortCountTotal = presenter.app.hillforts.size
+    var hillfortCountVisited = presenter.app.hillforts.count { hf -> hf.visited }
+    var hillfortCountFavorites = presenter.app.hillforts.count {hf -> hf.favorite }
+
+    settings_email.setBackground(null)
+    settings_email.setKeyListener(null)
+    settings_hillfort_count_total.setBackground(null)
+    settings_hillfort_count_total.setKeyListener(null)
+    settings_hillfort_count_visited.setBackground(null)
+    settings_hillfort_count_visited.setKeyListener(null)
+    settings_hillfort_count_favorite.setBackground(null)
+    settings_hillfort_count_favorite.setKeyListener(null)
+
+    val total = getResources().getString(R.string.settings_hillfort_count_total)
+    val visited = getResources().getString(R.string.settings_hillfort_count_visited)
+    val favorites = getResources().getString(R.string.settings_hillfort_count_favorite)
+
+    settings_email.setText(presenter.auth.currentUser?.email)
+    settings_hillfort_count_total.setText(hillfortCountTotal.toString() +" "+ total)
+    settings_hillfort_count_visited.setText(hillfortCountVisited.toString() +" "+ visited)
+    settings_hillfort_count_favorite.setText(hillfortCountFavorites.toString() +" "+ favorites)
+  }
+
+}
