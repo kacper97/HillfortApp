a793db3008d9f86466b4e216e0f91fbf9934457d
unknown <kacperwoloszyn4@gmail.com>
2019-01-04 20:07:12 +0000

    Hillfort in Database correct/prior layout changes

--- app/src/main/java/org/wit/hillfort/views/hillfort/HillfortView.kt
+++ app/src/main/java/org/wit/hillfort/views/hillfort/HillfortView.kt
@@ -21,7 +21,7 @@ class HillfortView : BaseView(), AnkoLogger, HillfortImageListener {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_hillfort)
 
-    init(toolbarAdd)
+    init(toolbarHillfort)
 
     presenter = initPresenter(HillfortPresenter(this)) as HillfortPresenter
 
@@ -40,9 +40,9 @@ class HillfortView : BaseView(), AnkoLogger, HillfortImageListener {
       }
     }
 
-    checkbox_visited.setOnClickListener {
+    hillfortVisited.setOnClickListener {
       tempSave()
-      presenter.doVisitedCheckbox(checkbox_visited.isChecked)
+      presenter.doVisitedCheckbox(hillfortVisited.isChecked)
     }
 
     hillfortFavorite.setOnClickListener {
@@ -51,15 +51,15 @@ class HillfortView : BaseView(), AnkoLogger, HillfortImageListener {
     }
 
     val layoutManager = androidx.recyclerview.widget.LinearLayoutManager(this, androidx.recyclerview.widget.LinearLayoutManager.HORIZONTAL, false)
-    hillfort_images_list_view.layoutManager = layoutManager
+    hillfortImageRecycler.layoutManager = layoutManager
     presenter.loadHillfortImages()
   }
 
   override fun showHillfort(hillfort: HillfortModel) {
     hillfortTitle.setText(hillfort.title)
-    description.setText(hillfort.description)
+    hillfortDescription.setText(hillfort.description)
     hillfortNotes.setText(hillfort.notes)
-    checkbox_visited.setChecked(hillfort.visited)
+    hillfortVisited.setChecked(hillfort.visited)
     hillfortFavorite.setChecked(hillfort.favorite)
     hillfortDate.setText(hillfort.date)
     hillfortRating.setRating(hillfort.rating)
@@ -69,12 +69,12 @@ class HillfortView : BaseView(), AnkoLogger, HillfortImageListener {
   }
 
   override fun showHillfortImages (images: List<String>) {
-    hillfort_images_list_view.adapter = HillfortImageAdapter(images, this)
-    hillfort_images_list_view.adapter?.notifyDataSetChanged()
+    hillfortImageRecycler.adapter = HillfortImageAdapter(images, this)
+    hillfortImageRecycler.adapter?.notifyDataSetChanged()
   }
 
   override fun onHillfortImageClick(image: String) {
-    toast("Image chosen!")
+    toast("Image pressed")
   }
 
   override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -87,17 +87,17 @@ class HillfortView : BaseView(), AnkoLogger, HillfortImageListener {
 
       R.id.item_save -> {
         var title = hillfortTitle.text.toString()
-        var description = description.text.toString()
+        var description = hillfortDescription.text.toString()
         var notes = hillfortNotes.text.toString()
-        var visited = checkbox_visited.isChecked
+        var visited = hillfortVisited.isChecked
         var favorite = hillfortFavorite.isChecked
         var date = hillfortDate.text.toString()
         var rating = hillfortRating.rating
 
         if (title.isEmpty()) {
-          toast(R.string.toast_enterTitlePlease)
+          toast(R.string.enter_hillfort_title)
         } else {
-          presenter.doAddOrSave(title, description, visited,notes, date,favorite , rating)
+          presenter.doAddOrSave(title, description,  visited,notes, date,favorite, rating)
         }
         setResult(AppCompatActivity.RESULT_OK)
         finish()
@@ -110,12 +110,12 @@ class HillfortView : BaseView(), AnkoLogger, HillfortImageListener {
     return super.onOptionsItemSelected(item)
   }
 
-  // Temporarily save hillfort in presenter hillfort object
+  // Temporarily save hillfort fields in presenter hillfort object
   fun tempSave(){
     var title = hillfortTitle.text.toString()
-    var description = description.text.toString()
+    var description = hillfortDescription.text.toString()
     var notes = hillfortNotes.text.toString()
-    var visited = checkbox_visited.isChecked
+    var visited = hillfortVisited.isChecked
     var favorite = hillfortFavorite.isChecked()
     var date = hillfortDate.text.toString()
     var rating = hillfortRating.rating
